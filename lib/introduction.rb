5faa5d3de8f2e7e19b2e10e5a1da0206996b67bb
bryan harrington <bryan17188@gmail.com>
2019-07-06 16:13:05 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,4 @@
-introduction_with_language("Dan", "Ember.js")}.to output("Hi, my name is Dan and I am learning to program in Ember.js.\n").to_stdout
+def introduction ("Dan", "Ember.js")}.to output("Hi, my name is Dan and I am learning to program in Ember.js.\n").to_stdout
 
 
 
