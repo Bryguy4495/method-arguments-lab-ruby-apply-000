dcec8bf8fba8d4ecdb7be97f9868c061f1963893
bryan harrington <bryan17188@gmail.com>
2019-07-06 16:44:19 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,9 +2,10 @@ def introduction (name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction (Dan,Ember.js)
+def introduction ('Dan','Ember.js')
   puts "Hi, my name is Dan and I am learning to program in Ember.js.\n"
 end
+
 
 
 
